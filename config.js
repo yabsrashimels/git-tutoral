4751059f44b68ee7eeef81f47b06f2379deddb33
yabsra <yabsrashimels531@gmail.com>
2025-12-13 23:25:11 +0300

    version 1

---
+++ config.js
@@ -0,0 +1,11 @@
+console.log("hi bro how are you");
+function fun1(a,b){
+    return a+b;
+}
+var arrow1 =(a,b)=> a+b;
+
+function fun2(){
+    console.log("welcome to simplicode");
+}
+fun2();
+fun1(1,2);
